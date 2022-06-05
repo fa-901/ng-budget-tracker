6a014c1c3c02580a1b463a5c3de78186d31ccf0f
Farhan Alam <metalhead901@live.com>
2022-06-05 18:27:08 +0600

    generate components with CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { ListGroupComponent } from './components/list-group/list-group.component';
+import { ButtonGroupComponent } from './components/button-group/button-group.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [
+		AppComponent,
+		FormInputComponent,
+		ListGroupComponent,
+		ButtonGroupComponent,
+	],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
