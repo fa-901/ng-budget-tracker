58f379be2d766cabaf69095d51f92b4bafc8b8c5
Farhan Alam <metalhead901@live.com>
2022-06-11 18:14:50 +0600

    update root app component

--- src/app/services/item-service.service.spec.ts
+++ src/app/services/item-service.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ItemServiceService } from './item-service.service';
 
 describe('ItemServiceService', () => {
-  let service: ItemServiceService;
+	let service: ItemServiceService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ItemServiceService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(ItemServiceService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
